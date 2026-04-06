406a39d33ad822b7ce48a86055e4cf0936586168
duong.ha <duong.ha@viaccess-orca.com>
2026-04-07 00:24:15 +0300

    Server: Logout API & authenticate middleware

--- hiloequation_server/src/auth/authUtils.js
+++ hiloequation_server/src/auth/authUtils.js
@@ -1,5 +1,16 @@
 'use strict'
 const JWT = require('jsonwebtoken');
+const { asyncHandler } = require('../helpers/asyncHandler');
+const { UnauthorizedError } = require('../core/error.response');
+const KeyTokenService = require('../services/keyToken.service');
+
+const HEADER = {
+    API_KEY: 'x-api-key',
+    CLIENT_ID: 'x-client-id',
+    AUTHORIZATION: 'authorization',
+    REFRESH_TOKEN: 'x-refresh-token',
+}
+
 const createTokenPair = async (payload, publicKey, privateKey) => {
     try {
         const accessToken = await JWT.sign(payload, privateKey, {
@@ -30,6 +41,46 @@ const createTokenPair = async (payload, publicKey, privateKey) => {
     }
 }
 
+const verifyJWT = async (token, keySecret) => {
+    return await JWT.verify(token, keySecret);
+}
+
+const authentication = asyncHandler(async (req, res, next) => {
+    const userId = req.headers[HEADER.CLIENT_ID];
+    if (!userId) throw new UnauthorizedError({ message: 'Invalid Request' });
+
+    const keyStore = await KeyTokenService.fincByUserId(userId);
+    if (!keyStore) throw new UnauthorizedError({ message: 'Keys Not Found' });
+
+    if (req.headers[HEADER.REFRESH_TOKEN]) {
+        try {
+            const refreshToken = req.headers[HEADER.REFRESH_TOKEN];
+            const decodeUser = await verifyJWT(refreshToken, keyStore.privateKey);
+            if (userId !== decodeUser.userId) throw new UnauthorizedError({ message: 'Invalid User Id' });
+            req.keyStore = keyStore;
+            req.user = decodeUser;
+            return next();
+        } catch (error) {
+            throw new UnauthorizedError({ message: error })
+        }
+    }
+    const accessToken = req.headers[HEADER.AUTHORIZATION];
+    if (!accessToken) throw new UnauthorizedError({ message: 'Invalid Request. No accessToken' });
+
+    try {
+        const decodeUser = await verifyJWT(accessToken, keyStore.privateKey);
+        if (userId !== decodeUser.userId) throw new UnauthorizedError({ message: 'Invalid User Id' });
+        req.keyStore = keyStore;
+        req.user = decodeUser;
+        console.log('decodeUser', decodeUser)
+        return next();
+    } catch (error) {
+        throw new UnauthorizedError({ message: error })
+    }
+});
+
 module.exports = {
-    createTokenPair
+    createTokenPair,
+    verifyJWT,
+    authentication,
 }

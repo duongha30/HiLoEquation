f69251bc8fb4d374d0889b54be8d913ef0ac15c2
duong.ha <duong.ha@viaccess-orca.com>
2026-06-03 21:11:53 +0300

    User: fix CSS player position and betting display

--- hiloequation_client/src/components/Player/index.tsx
+++ hiloequation_client/src/components/Player/index.tsx
@@ -23,6 +23,7 @@ export const Player = ({ id, position, additionalStyle }: PlayerProps) => {
     return (
         <div className={`${styles.container} ${additionalStyle ?? ''}`}>
             <div className={`${styles.cardsGroup} ${ROTATION[position]}`}>
+                <span style={{ position: 'absolute', left: 10, top: 0, zIndex: 1000 }}>{id}</span>
                 {normalizedCards.map(c => (
                     <div key={c.id} className={styles.cardWrapper}>
                         <Card card={c} faceDown={!!c.encryptedData} droppable={false} />
